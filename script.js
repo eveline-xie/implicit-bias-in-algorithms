eba9d74a1e3c0b3923ad39b856fb609aff44eb54
Glitch (hello-webpage) <none>
2020-08-21 20:42:20 +0000

    👨‍❤️‍👨💵 Checkpoint ./index.html:5668615/346 ./script.js:5668615/635 ./companies.html:5668615/668

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-function search_animal() { 
-    let input = document.getElementById('searchbar').value 
-    input=input.toLowerCase(); 
-    let x = document.getElementsByClassName('animals'); 
+// function search_bar() { 
+//     let input = document.getElementById('searchbar').value 
+//     input=input.toLowerCase(); 
+//     let x = document.getElementsByClassName('about'); 
       
-    for (var i = 0; i < x.length; i++) {  
-        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-            x[i].style.display="none"; 
-        } 
-        else { 
-            x[i].style.display="list-item";                  
-        } 
-    } 
-} 
+//     for (var i = 0; i < x.length; i++) {  
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+//             x[i].style.display="none"; 
+//         } 
+//         else { 
+//             x[i].style.display="list-item";                  
+//         } 
+//     } 
+// } 
+
+    function checkInput() {
+        var query = document.getElementById('search').value;
+        window.find(query);
+        return true;
+    }
