f2998e210c5112f6d476580b5321db07a40f8fa7
Glitch (hello-webpage) <none>
2020-08-21 20:54:20 +0000

    💺🐟 Checkpoint ./script.js:5668615/1839

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-// function search_bar() { 
-//     let input = document.getElementById('searchbar').value 
-//     input=input.toLowerCase(); 
-//     let x = document.getElementsByClassName('about'); 
-      
-//     for (var i = 0; i < x.length; i++) {  
-//         if (!x[i].innerHTML.toLowerCase().includes(input)) { 
-//             x[i].style.display="none"; 
-//         } 
-//         else { 
-//             x[i].style.display="list-item";                  
-//         } 
-//     } 
-// } 
+// function search_bar() {
+//     let input = document.getElementById('searchbar').value
+//     input=input.toLowerCase();
+//     let x = document.getElementsByClassName('about');
 
-    function checkInput() {
-        var query = document.getElementById('search').value;
-        window.find(query);
-        return true;
-    }
+//     for (var i = 0; i < x.length; i++) {
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display="none";
+//         }
+//         else {
+//             x[i].style.display="list-item";
+//         }
+//     }
+// }
+// function wait(ms) {
+//   var start = new Date().getTime();
+//   var end = start;
+//   while (end < start + ms) {
+//     end = new Date().getTime();
+//   }
+// }
+
+function checkInput() {
+  var query = document.getElementById("search").value;
+  window.find(query);
+  return true;
+}
