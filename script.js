5c6a2b0868a2dc96df7825dd0fa1d20b0d0d9c61
Glitch (hello-webpage) <none>
2020-08-22 18:19:41 +0000

    🍄🛢 Checkpoint ./index.html:21335779/27910 ./script.js:21335779/709 ./blm.html:21335779/44942 ./blm2.html:21335779/123 ./blm3.html:21335779/31011 ./companies.html:21335779/25099 ./style.css:21335779/5807

--- script.js
+++ script.js
@@ -47,26 +47,3 @@
 // }
 
 
-export default {
-    props: [
-      'site'
-    ],
-    data() {
-      return {
-        text: ''
-      }
-    },
-    methods: {
-      search(str) {
-        if (str) {
-          window.open([
-            'https://www.google.com/search?q=',
-            this.site ? 'site:' + this.site + '+' : '',
-            encodeURIComponent(str)
-          ].join(''));
-        } else {
-          this.$refs.input.focus();
-        }
-      }
-    }
-  }
